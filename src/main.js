c5d9575506b5625fe389679d68492705eb361b55
Seongho Park <bluetosh7@gmail.com>
2018-05-11 20:58:21 +0900

    feat: vuetify 및 기타 library들 추가

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
-
-Vue.config.productionTip = false
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import VueFire from 'vuefire'
+import '../node_modules/vuetify/src/stylus/app.styl'
 
 Vue.use(VueFire)
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }
+})
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
