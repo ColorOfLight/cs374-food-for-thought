432de4627aa638439418b336a766b3255dcc57a7
Seongho Park <bluetosh7@gmail.com>
2018-05-13 20:26:59 +0900

    fix: 디자인 및 코드 이슈 수정

--- src/pages/IngredientListPage.vue
+++ src/pages/IngredientListPage.vue
@@ -9,7 +9,7 @@
         .ingredient-list-item(v-for="(ingredient, key) in ingredients")
           .ingredient-title {{ingredient.name}}
           .ingredient-unit {{`${ingredient.unitPrice} 원 / ${ingredient.unit}`}}
-    empty-list-container(v-else text="메뉴에 들어갈 재료들을 추가해주세요!" :buttonText="buttonText" :clickRouteName="IngredientAdd")
+    empty-list-container(v-else text="메뉴에 들어갈 재료들을 추가해주세요!" :buttonText="buttonText" clickRouteName="IngredientAdd")
 </template>
 
 <script>
