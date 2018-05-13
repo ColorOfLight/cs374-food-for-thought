e2fed374c5925ba22b6081acbe3df7c8c70db7d7
WoongZ <chlwldnd3@naver.com>
2018-05-14 01:14:59 +0900

    fix: stringUtils 버그 수정

--- src/libs/stringUtils.js
+++ src/libs/stringUtils.js
@@ -2,7 +2,7 @@ const convertToMoneyString = (num, fixedNum = 0, suffix = " 원") => {
 
   if (typeof num !== 'number') num = parseInt(num);
   if (fixedNum === 0) {
-    return (num).toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1,')
+    return (num).toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1,') + suffix;
   } else {
     return (num).toFixed(fixedNum).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + suffix;
   }
