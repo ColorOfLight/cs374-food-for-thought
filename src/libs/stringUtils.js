ca3b39b85261928a7fc0abdec13979760fdc18d7
Seongho Park <bluetosh7@gmail.com>
2018-05-14 01:08:08 +0900

    fix: converToMoneyString fixedNum=0일 때 버그 수정

--- src/libs/stringUtils.js
+++ src/libs/stringUtils.js
@@ -1,7 +1,11 @@
 const convertToMoneyString = (num, fixedNum = 0, suffix = " 원") => {
 
   if (typeof num !== 'number') num = parseInt(num);
-  return (num).toFixed(fixedNum).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + suffix;
+  if (fixedNum === 0) {
+    return (num).toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1,')
+  } else {
+    return (num).toFixed(fixedNum).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + suffix;
+  }
 }
 
 export {
